0f76ed0985003a84bbf2cfa63b13078cbb48fc2a
Feather Knee <featherknee@gmail.com>
2017-05-13 15:26:41 -0700

    Name app something I will remember in 2 months.

--- config/prod.exs
+++ config/prod.exs
@@ -2,7 +2,7 @@ use Mix.Config
 
 config :chatter, Chatter.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [scheme: "https", host: "enigmatic-fortress-52590.herokuapp.com/", port: 443],
+  url: [scheme: "https", host: "elixir-chatter.herokuapp.com/", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
