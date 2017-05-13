ba9058bc5dfb22e3b1f43bb007b5e9b461c376a7
Feather Knee <featherknee@gmail.com>
2017-05-13 14:25:35 -0700

    Add prod vars and whatnot

--- web/channels/user_socket.ex
+++ web/channels/user_socket.ex
@@ -6,7 +6,7 @@ defmodule Chatter.UserSocket do
   channel "chat_room:lobby", Chatter.ChatRoomChannel
 
   ## Transports
-  transport :websocket, Phoenix.Transports.WebSocket
+  transport :websocket, Phoenix.Transports.WebSocket, timeout: 45_000
   # transport :longpoll, Phoenix.Transports.LongPoll
 
   # Socket params are passed from the client and can
